d0d8390ce9a2383dc91f5088ab0e9e3117edc62a
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-23 22:42:55 +0600

    contact

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -1,22 +1,24 @@
 import React from "react";
-import appointment from "../../assets/images/appointment.png";
+import bgimg from "../../assets/images/banner1.jpg";
 import PrimaryButton from "../Shared/PrimaryButton";
 
 const Contact = () => {
   return (
     <div
       style={{
-        background: `url(${appointment})`,
+        background: `url(${bgimg})`,
       }}
       className="bg-primary px-10 py-14 "
     >
       <div className="text-center pb-14 text-white">
-        <p className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary">
-          Contact Us
-        </p>
-        <h1 className="text-4xl">Stay connected with us</h1>
+        <h1 className="text-4xl">CONTACT ME</h1>
       </div>
       <div className="grid grid-cols-1 justify-items-center gap-5">
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="input w-full max-w-md"
+        />
         <input
           type="text"
           placeholder="Email Address"
@@ -24,12 +26,12 @@ const Contact = () => {
         />
         <input
           type="text"
-          placeholder="Subject"
+          placeholder="Problem"
           className="input w-full max-w-md"
         />
         <textarea
           className="textarea w-full max-w-md"
-          placeholder="Your message"
+          placeholder="Details"
           rows={6}
         ></textarea>
         <PrimaryButton>Submit</PrimaryButton>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,7 +5,6 @@ import Footer from '../Shared/Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
-import Testimonials from './Testimonials';
 import Treatmant from './Treatmant';
 import Business_summary from '../Business_summary/Business_summary';
 
@@ -18,7 +17,6 @@ const Home = () => {
            <Services></Services>
            <Treatmant></Treatmant>
            <MakeAppointment></MakeAppointment>
-           <Testimonials></Testimonials>
            <Contact></Contact>
            <Footer></Footer>
         </div>
