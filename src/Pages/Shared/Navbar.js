531d66cbd24119a99e8c374bf91a793fde991d8d
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-24 00:14:16 +0600

    uisness summary

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-500">
+    <div className="navbar ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex="0" className="btn btn-ghost lg:hidden">
