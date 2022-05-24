801f7668b6bfaf70a9f52e4c6c497f3f6e8b9f8c
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-24 22:42:39 +0600

    add dashbord

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -46,6 +46,7 @@ const MyOrder = () => {
               <th>Quantity</th>
               <th>Total Price</th>
               <th>Payment</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -75,6 +76,37 @@ const MyOrder = () => {
                     </div>
                   )}
                 </td>
+                <td>
+                  <label
+                    disabled={a.totalprice && a.paid}
+                    for="booking-modal"
+                    class="btn btn-xs text-white"
+                  >
+                    Cancel
+                  </label>
+                </td>
+
+                <input
+                  type="checkbox"
+                  id="booking-modal"
+                  class="modal-toggle"
+                />
+                <div class="modal modal-bottom sm:modal-middle">
+                  <div class="modal-box">
+                    <h3 class="font-bold text-lg">
+                      Congratulations random Interner user!
+                    </h3>
+                    <p class="py-4">
+                      You've been selected for a chance to get one year of
+                      subscription to use Wikipedia for free!
+                    </p>
+                    <div class="modal-action">
+                      <label for="booking-modal" class="btn">
+                        Yay!
+                      </label>
+                    </div>
+                  </div>
+                </div>
               </tr>
             ))}
           </tbody>
